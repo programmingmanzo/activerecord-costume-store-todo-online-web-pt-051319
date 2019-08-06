c191b16c69c10f845ed02bafbae095f3ca3af7a0
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-06 20:05:45 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,4 +1,6 @@
 
-
-class Costume < ActiveRecord:# Create your Costume class here
+# Create your Costume class here
+class Costume < ActiveRecord::Base 
+  
+end
 # It should inherit from ActiveRecord::Base
